63ce4170a4916805edb296c15557310ad43b34f1
i2gun <i2gun@bk.ru>
2024-02-13 22:11:56 +0600

    Separating all the tests into Test modules

--- Test/seminars/second/hw/VehicleTest.java
+++ Test/seminars/second/hw/VehicleTest.java
@@ -9,29 +9,48 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class VehicleTest {
 
-    public static void main(String[] args) {
+    Vehicle car = new Car("Volkswagen", "Golf", 2014);
+    Vehicle moto = new Motorcycle("Suzuki", "GSX-S1000", 2015);
 
-        Vehicle car = new Car("Volkswagen", "Golf", 2014);
-        Vehicle moto = new Motorcycle("Suzuki", "GSX-S1000", 2015);
-
+    @Test
+    void testClassCarExtendsVehicle() {
         assertInstanceOf(Vehicle.class, car);
+    }
 
+    @Test
+    void testCarHasFourWheels() {
         assertEquals(4, ((Car) car).getNumWheels());
+    }
 
+    @Test
+    void testMotorcycleHasTwoWheels() {
         assertEquals(2, ((Motorcycle) moto).getNumWheels());
+    }
 
+    @Test
+    void testTestDriveSpeedOfCar() {
         car.testDrive();
         assertEquals(60, ((Car) car).getSpeed());
+    }
 
+    @Test
+    void testTestDriveSpeedOfMotorcycle() {
         moto.testDrive();
         assertEquals(75, ((Motorcycle) moto).getSpeed());
+    }
 
+    @Test
+    void testParkingSpeedOfCar() {
         car.testDrive();
         car.park();
         assertEquals(0, ((Car) car).getSpeed());
+    }
 
+    @Test
+    void testParkingSpeedOfMotorcycle() {
         moto.testDrive();
         moto.park();
         assertEquals(0, ((Motorcycle) moto).getSpeed());
     }
+
 }
